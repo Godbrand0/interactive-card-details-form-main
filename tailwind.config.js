f461d3d40ec2df607a81eb5d099ada2d120e5684
Godbrand0 <140930314+Godbrand0@users.noreply.github.com>
2024-06-19 07:09:53 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        boxShadow: {
+          '3xl': '20 25px 50px 12px rgb(0 0 0 / 0.25);',
+        },
+        fontFamily: {
+            space: ['"Space Grotesk"' ]
+        }
+      }
+    }
+}
